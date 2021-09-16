3628aad7486a2912bf3a59aa4f262b30de107252
swdpcomputing <swdpcomputing@outlook.com>
2021-09-16 14:28:45 +0100

    feat(sso): add sso

--- services/sso/src/lib/sso.js
+++ services/sso/src/lib/sso.js
@@ -1,24 +1,23 @@
 const msal = require("@azure/msal-node");
+const ensureIsAuthenticated = require("../middleware/ensure-is-authenticated");
 
-const getAzureConfig = (authConfig, logger) => {
-  return {
-    auth: {
-      clientId: authConfig.clientId,
-      authority: `${authConfig.cloudInstanceId}/${authConfig.tenantId}`,
-      clientSecret: authConfig.clientSecret,
-    },
-    system: {
-      loggerOptions: {
-        /* istanbul ignore next */
-        loggerCallback(loglevel, message) {
-          logger.info({ loglevel, message }, `Logger callback`);
-        },
-        piiLoggingEnabled: false,
-        logLevel: msal.LogLevel.Verbose,
+const getAzureConfig = (authConfig, logger) => ({
+  auth: {
+    clientId: authConfig.clientId,
+    authority: `${authConfig.cloudInstanceId}/${authConfig.tenantId}`,
+    clientSecret: authConfig.clientSecret,
+  },
+  system: {
+    loggerOptions: {
+      /* istanbul ignore next */
+      loggerCallback(loglevel, message) {
+        logger.info({ loglevel, message }, `Logger callback`);
       },
+      piiLoggingEnabled: false,
+      logLevel: msal.LogLevel.Verbose,
     },
-  };
-};
+  },
+});
 
 module.exports = (app, config, logger) => {
   const cca = new msal.ConfidentialClientApplication(
@@ -26,20 +25,24 @@ module.exports = (app, config, logger) => {
   );
 
   /* istanbul ignore next */
-  app.get("/", (req, res) => {
-    const authCodeUrlParameters = {
-      scopes: ["user.read"],
-      redirectUri: config.redirectUri,
-    };
+  app.use("/", (req, res, next) => {
+    if (req.session.isAuthenticated !== true) {
+      const authCodeUrlParameters = {
+        scopes: ["user.read"],
+        redirectUri: config.redirectUri,
+      };
 
-    cca
-      .getAuthCodeUrl(authCodeUrlParameters)
-      .then((response) => {
-        res.redirect(response);
-      })
-      .catch((error) =>
-        logger.error({ error }, `Error getting authorisation url`)
-      );
+      cca
+        .getAuthCodeUrl(authCodeUrlParameters)
+        .then((response) => {
+          req.session.isAuthenticated = true;
+          res.redirect(response);
+        })
+        .catch((error) =>
+          logger.error({ error }, `Error getting authorisation url`)
+        );
+    }
+    next();
   });
 
   /* istanbul ignore next */
@@ -61,4 +64,6 @@ module.exports = (app, config, logger) => {
         res.status(500).send(error);
       });
   });
+
+  app.use("/", ensureIsAuthenticated);
 };
