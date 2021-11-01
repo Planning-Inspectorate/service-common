e85dc387363d202fd4c78cfba260251c58142e71
Demzy <79277486+demzzydev@users.noreply.github.com>
2021-11-01 14:07:39 +0000

    feat(as-3650): reenable clam av + add custom port & host

--- services/clamav/src/index.js
+++ services/clamav/src/index.js
@@ -2,27 +2,29 @@
 const NodeClam = require("clamscan");
 const fs = require("fs");
 
+const { CLAM_AV_PORT, CLAM_AV_HOST } = process.env
+
 module.exports = async (fileInformation, fileName = "The file") => {
-  // if (typeof fileInformation?.tempFilePath !== "undefined") {
-  //   const clamscan = await new NodeClam().init({
-  //     debug_mode: true,
-  //     clamdscan: {
-  //       host: "clamav",
-  //       port: 3310,
-  //       bypass_test: true,
-  //     },
-  //   });
-  //
-  //   const fileStream = fs.createReadStream(fileInformation.tempFilePath);
-  //
-  //   // eslint-disable-next-line camelcase
-  //   const { is_infected } = await clamscan.scan_stream(fileStream);
-  //
-  //   // eslint-disable-next-line camelcase
-  //   if (is_infected) {
-  //     throw new Error(`${fileName} contains a virus`);
-  //   }
-  // }
+  if (typeof fileInformation?.tempFilePath !== "undefined") {
+    const clamscan = await new NodeClam().init({
+      debug_mode: true,
+      clamdscan: {
+        host: CLAM_AV_HOST || "clamav",
+        port: CLAM_AV_PORT || 3310,
+        bypass_test: true,
+      },
+    });
+  
+    const fileStream = fs.createReadStream(fileInformation.tempFilePath);
+  
+    // eslint-disable-next-line camelcase
+    const { is_infected } = await clamscan.scan_stream(fileStream);
+  
+    // eslint-disable-next-line camelcase
+    if (is_infected) {
+      throw new Error(`${fileName} contains a virus`);
+    }
+  }
 
   return true;
 };
