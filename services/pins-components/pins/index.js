189d23767a42534c0f062c912403bcc736d9d362
Andy Langridge <andy.langridge@kinandcarta.com>
2022-07-05 11:47:28 +0100

    fix: invalid main entry in package.json

---
+++ services/pins-components/pins/index.js
@@ -0,0 +1,11 @@
+const contentsSidebar = require('./components/contents-sidebar');
+const footer = require('./components/footer');
+const header = require('./components/header');
+const stepByStepNavigation = require('./components/step-by-step-navigation');
+
+module.exports = {
+  contentsSidebar,
+  footer,
+  header,
+  stepByStepNavigation
+};
