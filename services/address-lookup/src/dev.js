eb5fbdd143bd4523eda28b607d4fa3892bd37d07
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-10-16 09:17:40 +0100

    chore: consolidate eslint and prettier to top-level

--- services/address-lookup/src/dev.js
+++ services/address-lookup/src/dev.js
@@ -2,7 +2,6 @@ const logger = require("./utils/logger");
 const findAddressListByPostcode = require("./services/findAddressListByPostcode");
 
 (async () => {
-  // eslint-disable-next-line prefer-destructuring
   process.env.OS_PLACES_API_KEY = process.argv[2];
   const results = await findAddressListByPostcode("EC2M 4PT", {
     maxResults: 2,
